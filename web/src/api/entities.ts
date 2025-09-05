1734d47963ce913af5c89f7c352abf666fdc6fb5
BlueSkyFish <sarah.kirchner@proton.me>
2025-09-05 09:19:07 -0700

    feat: 🔗  refactor to "api"

---
+++ web/src/api/entities.ts
@@ -0,0 +1,24 @@
+export interface BranchDto {
+  branch: string;
+  place: string;
+  active: boolean;
+}
+
+export interface ProjectDto {
+  id: string;
+  name: string;
+  description: string;
+  path: string;
+  gitUrl: string;
+  branch: string;
+  user: string;
+  creation: string;
+  modified: string;
+  status: string;
+  commandMap: Record<string, boolean>;
+
+  /**
+   * Optional the list of branches for the project repository
+   */
+  branchList?: BranchDto[];
+}
---
+++ web/src/api/index.ts
@@ -0,0 +1,4 @@
+export * from './entities';
+export * from './fetch-user';
+export * from './fetch-project-list';
+export * from './fetch-branch-list';
